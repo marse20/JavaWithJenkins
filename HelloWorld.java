745e230b167e3d88ced42ba7441bd13d876d7b71
marse20 <manish20vns@gmail.com>
2018-04-08 02:09:30 +0530

    Add second print

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld {
 	public static void main(String arg[]) {
 		for(int i = 0; i < 10; i++) {
 			System.out.println(i+1+": HelloWorld program run by Jenkins");
+			System.out.println("Second line of code\n");
 		}
 	}
 }
