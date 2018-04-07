573e143dec04bd71eded812fd2e5c1322b2e1892
marse20 <manish20vns@gmail.com>
2018-04-08 02:35:37 +0530

    remove second line

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,6 @@ public class HelloWorld {
 	public static void main(String arg[]) {
 		for(int i = 0; i < 10; i++) {
 			System.out.println(i+1+": HelloWorld program run by Jenkins");
-			System.out.println("Second line of code\n");
 		}
 	}
 }
